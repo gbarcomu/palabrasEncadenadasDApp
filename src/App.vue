ed4fca966a106c04ac618decfaf297fc0c70b2af
Guillermo <gbarcomu@gmail.com>
2018-07-26 12:00:27 -0500

    primera version que funciona

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/ust.jpeg">
+    <h1> Juego de palabras encadenadas mediante una DApp </h1>
     <HelloWorld/>
   </div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ currentWord }}</h1>
     <h2>Essential Links</h2>
 <button v-on:click="callContract">Call</button>
+<input v-model="newWord" placeholder="nueva palabra">
+<button v-on:click="sendContract">Send</button>
   </div>
 </template>
 
@@ -11,12 +13,12 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      currentWord: "",
+      newWord: ""
     };
   },
   methods: {
     callContract() {
-
       var contractFactory = new web3.eth.Contract(
         [
           {
@@ -67,13 +69,74 @@ export default {
       );
 
       contractFactory.methods["getPalabra"]()
-      .call({from: "0x29577899A8c5A8c4352737f3CC09c7627F6c3101"})
-      .then(console.log)
+        .call({ from: "0x29577899A8c5A8c4352737f3CC09c7627F6c3101" })
+        .then(result => {
+          this.currentWord = result;
+        });
+    },
+
+    sendContract() {
+      var contractFactory = new web3.eth.Contract(
+        [
+          {
+            constant: false,
+            inputs: [
+              {
+                name: "primeraLetra",
+                type: "string"
+              },
+              {
+                name: "nuevaPalabra",
+                type: "string"
+              }
+            ],
+            name: "setNuevaPalabra",
+            outputs: [
+              {
+                name: "",
+                type: "bool"
+              }
+            ],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function"
+          },
+          {
+            inputs: [],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "constructor"
+          },
+          {
+            constant: true,
+            inputs: [],
+            name: "getPalabra",
+            outputs: [
+              {
+                name: "",
+                type: "string"
+              }
+            ],
+            payable: false,
+            stateMutability: "view",
+            type: "function"
+          }
+        ],
+        "0xb702142943e6065630cd7015e544f7acd37b397b"
+      );
+
+      contractFactory.methods["setNuevaPalabra"](this.newWord[0], this.newWord)
+        .send({ from: "0x29577899A8c5A8c4352737f3CC09c7627F6c3101" })
+        .then(result => {
+          if (this.newWord[0] == this.currentWord[this.currentWord.length - 1]) {
+            this.currentWord = this.newWord;
+          }
+          else {
+            alert("La primera letra de la última palabra no coincide con la anterior");
+          }
+        });
     }
   }
-  // mounted() {
-  //   this.loadContract();
-  // }
 };
 </script>
 
